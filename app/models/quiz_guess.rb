9458cebd1d4b57b1d631acabae7becdca122b476
B.J. Allen <bjallen@mac.com>
2022-06-04 09:20:28 -0500

    adds correctness check on guesses

--- app/models/quiz_guess.rb
+++ app/models/quiz_guess.rb
@@ -2,4 +2,9 @@ class QuizGuess < ApplicationRecord
   belongs_to :behavior
   belongs_to :level
   belongs_to :competency
+  
+  def correct?
+    behavior.competency_id == competency_id &&
+      behavior.level_id == level_id
+  end
 end
---
+++ spec/models/quiz_guess_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe QuizGuess, type: :model do
+  it "is correct if the IDs match up" do
+    behavior = Behavior.new(id: 1, competency_id: 34, level_id: 8)
+    guess = QuizGuess.new(behavior: behavior, competency_id: 34, level_id: 8)
+    expect(guess).to be_correct
+  end
+  
+  it "is not correct if the level is incorrect" do
+    behavior = Behavior.new(id: 1, competency_id: 34, level_id: 8)
+    guess = QuizGuess.new(behavior: behavior, competency_id: 34, level_id: 3)
+    expect(guess).to_not be_correct
+  end
+  
+  it "is not correct if the competency is incorrect" do
+    behavior = Behavior.new(id: 1, competency_id: 34, level_id: 8)
+    guess = QuizGuess.new(behavior: behavior, competency_id: 4, level_id: 8)
+    expect(guess).to_not be_correct
+  end
+end
