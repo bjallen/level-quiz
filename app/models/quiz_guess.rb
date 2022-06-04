f8786227b7ea4048a349d717341c457fbbc52d89
B.J. Allen <bjallen@mac.com>
2022-06-04 17:21:15 -0500

    adds rubocop config

--- app/models/quiz_guess.rb
+++ app/models/quiz_guess.rb
@@ -2,7 +2,7 @@ class QuizGuess < ApplicationRecord
   belongs_to :behavior
   belongs_to :level
   belongs_to :competency
-  
+
   def correct?
     behavior.competency_id == competency_id &&
       behavior.level_id == level_id
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :levels
   resources :competencies
   resources :pillars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
   get "auth/:provider/callback", to: "sessions#create"
   get "/login", to: "sessions#new"
 
